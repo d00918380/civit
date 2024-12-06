6e0ea86703d7abc1f0563a273572ac23dd9d6a47
0dang <d00918380@gmail.com>
2024-12-06 20:06:55 +1100

    add users download <username>

--- internal/civit/image.go
+++ internal/civit/image.go
@@ -1,6 +1,13 @@
 package civit
 
 type Image struct {
-	Id  int    `json:"id"`
-	Url string `json:"url"`
+	Id        int    `json:"id"`
+	CreatedAt string `json:"createdAt"`
+	Url       string `json:"url"`
+	PostId    int    `json:"postId"`
+	Username  string `json:"username"`
+}
+
+type Metadata struct {
+	NextPage string `json:"nextPage"`
 }
--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
+	"time"
 
 	"github.com/alecthomas/kong"
+	"github.com/carlmjohnson/requests"
 	"github.com/d00918380/civit/internal/civit"
 )
 
 var CLI struct {
-	APIKey string `env:"CIVIT_API_KEY" help:"API key."`
+	APIKey string `env:"CIVIT_API_KEY" help:"API key." required:""`
 	Posts  struct {
 		Download struct {
 			Ids []int `arg:"" name:"id" help:"Post IDs to download."`
 		} `cmd:"" help:"Download posts."`
 	} `cmd:"" help:"Manage posts."`
+	Users struct {
+		Download struct {
+			Username string `arg:"" name:"username" help:"Username to download."`
+		} `cmd:"" help:"Download users."`
+	} `cmd:"" help:"Manage users."`
 }
 
 func main() {
@@ -28,8 +38,56 @@ func run() error {
 	ctx := kong.Parse(&CLI)
 	switch ctx.Command() {
 	case "posts download <id>":
-		return civit.NewPosts(civit.NewClient(CLI.APIKey)).Download(CLI.Posts.Download.Ids...)
+		for _, id := range CLI.Posts.Download.Ids {
+			url := "https://civitai.com/api/v1/images?nsfw=X&token=" + CLI.APIKey + "&postId=" + fmt.Sprintf("%d", id)
+			if err := downloadImages(context.Background(), url); err != nil {
+				return err
+			}
+		}
+		return nil
+	case "users download <username>":
+		url := "https://civitai.com/api/v1/images?nsfw=X&token=" + CLI.APIKey + "&username=" + CLI.Users.Download.Username
+		return downloadImages(context.Background(), url)
 	default:
 		return fmt.Errorf("unknown command: %s", ctx.Command())
 	}
+}
+
+func downloadImages(ctx context.Context, url string) error {
+	var resp struct {
+		Items    []*civit.Image
+		Metadata civit.Metadata `json:"metadata"`
+	}
+	err := requests.URL(url).ToJSON(&resp).Fetch(ctx)
+	if err != nil {
+		return err
+	}
+	for _, item := range resp.Items {
+		path := imageToPath(item)
+		if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+			return err
+		}
+		fmt.Fprintf(os.Stderr, "Downloading %s to %s\n", item.Url, path)
+		if err := requests.URL(item.Url).ToFile(path).Fetch(ctx); err != nil {
+			return err
+		}
+		ctime, err := time.Parse(time.RFC3339, item.CreatedAt)
+		if err != nil {
+			return err
+		}
+		if err := os.Chtimes(path, ctime, ctime); err != nil {
+			return err
+		}
+		if err := os.Chtimes(filepath.Dir(path), ctime, ctime); err != nil {
+			return err
+		}
+	}
+	if resp.Metadata.NextPage != "" {
+		return downloadImages(ctx, resp.Metadata.NextPage)
+	}
+	return nil
+}
+
+func imageToPath(image *civit.Image) string {
+	return filepath.Join("posts", image.Username, fmt.Sprintf("%d", image.PostId), path.Base(image.Url))
 }
